97c2bf88870ad7766817c9e778995c6ba46d910c
Ivano Donadi <ivadonadi98@gmail.com>
2021-06-20 21:45:18 +0200

    Update BBoxSelector.cpp (#15) (#16)
    Co-authored-by: Samuele Benfatti <76395249+BenfattiS@users.noreply.github.com>

--- BBoxSelector.cpp
+++ BBoxSelector.cpp
@@ -60,6 +60,62 @@ int main(int argc, char** argv)
 			}
 		}
 	}
+	else if(argc == 3 and strcmp(argv[2],"-R")==0)
+	{
+		for(const auto& fn: filenames)
+		{
+			cv::String baseFn = fn.substr(0,fn.size()-4);
+			std::vector<cv::Rect> ROIs;
+			cv::Mat img = cv::imread(fn);
+			
+			for(const auto& dn: done)
+			{
+				if(
+					fn.size() == dn.size() and
+					baseFn == dn.substr(0,dn.size()-4)
+				)
+				{
+					std::ifstream input;
+					input.open(dn);
+					int x, y, w, h;
+					while (input >> x >> y >> w >> h)
+					{
+						std::cout<<x<<" "<<y<<" "<<w<<" "<<h<<"\n";
+						cv::Rect ROI = cv::Rect(x,y,w,h);
+						ROIs.push_back(ROI);
+						cv::rectangle(img,ROI,cv::Scalar(0,0,255));
+					}
+					std::cout<<"\n";
+					input.close();
+					break;
+				}
+			}
+
+			bool loop = true;
+			while(loop)
+			{
+				cv::Rect ROI = cv::selectROI("BBoxSelector", img, false);
+				if(ROI.x == 0 and ROI.y == 0 and ROI.width == 0 and ROI.height == 0)
+				{
+					loop = false;
+				}
+				else
+				{
+					cv::rectangle(img,ROI,cv::Scalar(0,0,255));
+					ROIs.push_back(ROI);
+				}
+			}
+			std::cout<<"\n"<<ROIs.size()<<" ROIs selected\n\n\n";
+			
+			std::ofstream output;
+			output.open(baseFn+".txt");
+			for(const auto& ROI: ROIs)
+			{
+				output<<ROI.x<<" "<<ROI.y<<" "<<ROI.width<<" "<<ROI.height<<"\n";
+			}
+			output.close();
+		}
+	}
 	else
 	{
 		for(const auto& fn: filenames)
