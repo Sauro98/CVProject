3aa1144b352d0775da4daa308da1d547c9d1a08f
Ivano Donadi <ivadonadi98@gmail.com>
2021-06-24 14:21:27 +0200

    B&W path change (#28)
    * cv createDir
    
    * fix path
    
    * Update BlackWhite_He.h
    
    Ho aggiunto le altre due funzioni come richiesto
    
    * Update BlackWhite_He.cpp
    
    Co-authored-by: annazuccante <82164631+annazuccante@users.noreply.github.com>

--- BlackWhite_He.cpp
+++ BlackWhite_He.cpp
@@ -7,34 +7,26 @@
 using namespace cv;
 using namespace std;
 
-#if __cplusplus < 201703L // If the version of C++ is less than 17
-#include <experimental/filesystem>
-// It was still in the experimental:: namespace
-namespace fs = std::experimental::filesystem;
-
-
-#else
-#include <filesystem>
-    namespace fs = std::filesystem;
-
-#endif
-
-
-
 
 int main(int argc, char** argv) {
 
 
-        if (argc != 3) {
-            printf("usage: ./filename <Image_Directory> <Format_of_images: ex. *.bmp or *.png or *.jpg>\n");
-            return -1;
+       if (argc != 3 ) {
+            if(argc == 4)
+            {}
+            else
+            {
+                    printf("usage: ./filename <Image_Directory> <Format_of_images: ex. *.bmp or *.png> <Optional command (-hsv or -rgb)>\n");
+                    return -1;
+            }
         }
 
     std::vector<cv::String> filenames;
     std::vector<cv::String> output_names;
     std::vector<cv::Mat> images;
-    string newfolder = "B&W_" + String(argv[1]);
-    fs::create_directory(newfolder);
+    Mat result_img;
+    string optionalcmd; //the 4th optional string on the command line
+    
 
     cv::utils::fs::glob( String(argv[1]), String(argv[2]), filenames);
 
@@ -48,25 +40,63 @@ int main(int argc, char** argv) {
 
         //create a new object of the class BlackWhite_He
         BlackWhite_He preproc_img = BlackWhite_He();
-        Mat result_img = preproc_img.bgr_to_gray_HE(img);
+            
+        //calling functions of the class
+        if(argc == 4 and strcmp(argv[3],"-rgb")==0)
+        {
+            optionalcmd = "RGBhe_";
+            result_img = preproc_img.bgr_HE_to_gray(img);
+            images.push_back(result_img);
+            int beg_str = String(argv[1]).size() + 1;
+            int len_str = (int)fn.size()-(4+beg_str);
+            String name =  optionalcmd + "BnW_" + fn.substr(beg_str,len_str);
+            output_names.push_back(name);
+        }
 
-        images.push_back(result_img);
-        int beg_str = String(argv[1]).size();
-        int len_str = (int)fn.size()-(4+beg_str);
-        String name =  "B&W_" + fn.substr(beg_str,len_str);
-        output_names.push_back(name);
+        else if(argc == 4 and strcmp(argv[3],"-hsv")==0)
+        {
+            optionalcmd = "HSVhe_";
+            result_img = preproc_img.hsv_HE_to_gray(img);
+            images.push_back(result_img);
+            int beg_str = String(argv[1]).size() + 1;
+            int len_str = (int)fn.size()-(4+beg_str);
+            String name =  optionalcmd + "BnW_" + fn.substr(beg_str,len_str);
+            output_names.push_back(name);
+        }
+
+        else
+        {
+            optionalcmd = "";
+            result_img = preproc_img.bgr_to_gray_HE(img);
+            images.push_back(result_img);
+            int beg_str = String(argv[1]).size() + 1;
+            int len_str = (int)fn.size()-(4+beg_str);
+            String name =  optionalcmd + "BnW_" + fn.substr(beg_str,len_str);
+            output_names.push_back(name);
+        }
 
     }
-    stringstream ss;
+        
+    //creation of a folder to contain the new images    
+    string newfolder = String(argv[1])  + "/"+ optionalcmd + "BnW";
+    std::cout<<newfolder<<std::endl;
+    if(!cv::utils::fs::createDirectory(newfolder)){
+        std::cout<<"Failed to create directory"<<std::endl;
+    }
+    
+    //writing the images resulting on the new folder
     for(int i = 0; i<output_names.size(); i++)
     {
-        cout<<output_names[i]<<endl;
+        stringstream ss;
         ss<<newfolder<<"/"<<output_names[i]<<String(argv[2]).substr(1);
+        string fullPath = ss.str();
+        cout<<fullPath<<endl;
+        ss.str("");
 
-    string fullPath = ss.str();
-    ss.str("");
-
-    imwrite(fullPath, images[i]);}
+        if(!imwrite(fullPath, images[i])){
+            std::cout<<"Failed to write image"<<std::endl;
+        }
+    }
 
     return 0;
 }
