83804875c6102882ede92d1921be01f3cb2493a9
Samuele Benfatti <76395249+BenfattiS@users.noreply.github.com>
2021-06-24 14:53:50 +0200

    Interface

--- TestAnn.cpp
+++ TestAnn.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 	topology.push_back(2);
 	topology.push_back(5);
 	topology.push_back(5);
-	topology.push_back(1);
+	topology.push_back(2);
 	Ann ann = Ann(topology);
 	
 	ann.show();
@@ -26,40 +26,33 @@ int main(int argc, char** argv)
 			inputs.push_back(tmp);
 			tmp.clear();
 			tmp.push_back(1-(i==j));
+			tmp.push_back((i==j));
 			outputs.push_back(tmp);
 			ann.use(inputs[inputs.size()-1],tmp);
-			std::cout<<i<<" "<<j<<": "<<tmp[0]<<" ("<<outputs[outputs.size()-1][0]<<")\n";
+			std::cout<<i<<" "<<j<<": "<<tmp[0]<<", "<<tmp[1]<<" ("<<outputs[2*i+j][0]<<", "<<outputs[2*i+j][1]<<")\n";
 		}
 	}
 	
 	std::cout<<"Error: "<<ann.getError(inputs,outputs)<<"\n";
+	std::cout<<"Categorical: "<<ann.getCategoricalError(inputs,outputs)*100<<"%\n";
 	
 	std::cout<<"\nTraining....\n\n";
-	ann.train(inputs,outputs,inputs,outputs, true,
-				0.001, 1000);
+	ann.train(inputs,outputs,inputs,outputs,
+				0.001, 10000, 20, -1, false);
 	
 	for(int i=0; i<2; ++i)
 	{
 		for(int j=0; j<2; ++j)
 		{
-			std::vector<double> tmp(1,0);
+			std::vector<double> tmp(2,0);
 			ann.use(inputs[2*i+j],tmp);
-			std::cout<<i<<" "<<j<<": "<<tmp[0]<<" ("<<outputs[2*i+j][0]<<")\n";
+			std::cout<<i<<" "<<j<<": "<<tmp[0]<<", "<<tmp[1]<<" ("<<outputs[2*i+j][0]<<", "<<outputs[2*i+j][1]<<")\n";
 		}
 	}
 	std::cout<<"Error: "<<ann.getError(inputs,outputs)<<"\n";
+	std::cout<<"Categorical: "<<ann.getCategoricalError(inputs,outputs)*100<<"%\n";
 	
 	ann.show();
-	
-	
-	//std::vector<double> inputs;
-	//std::vector<double> outputs(1,0.0);
-	//inputs.push_back(1.0);
-	//inputs.push_back(-1.0);
-	//ann.show();
-	//ann.use(inputs,outputs);
-	//ann.show();
-	//std::cout<<"Output: "<<outputs[0]<<"\n";
 	
 //	bool saved = ann.save("../test_ann");
 //	std::cout<<"Saved: "<<saved<<"\n";
