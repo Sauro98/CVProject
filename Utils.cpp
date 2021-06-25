f56369573166ac5fb9c5c0fe92faa2e7ea2d275f
Samuele Benfatti <76395249+BenfattiS@users.noreply.github.com>
2021-06-25 10:02:59 +0200

    Added segmentation utils

--- Utils.cpp
+++ Utils.cpp
@@ -12,7 +12,6 @@ void loadROIs(std::string name, std::vector<cv::Rect>& ROIs)
 	input.close();
 }
 
-
 void selectROIs(cv::String name, cv::Mat& img, std::vector<cv::Rect>& ROIs)
 {
 	bool loop = true;
@@ -59,4 +58,270 @@ void drawROIs(cv::Mat& img, std::vector<cv::Rect>& ROIs)
 	{
 		cv::rectangle(img,ROI,cv::Scalar(0,0,255));
 	}
+}
+
+void bgr_to_gray_HE(cv::Mat& image, cv::Mat& out)
+{
+	// Created by Anna Zuccante on 20/06/2021.
+	cv::Mat gray_img;
+	cv::cvtColor(image, gray_img, cv::COLOR_BGR2GRAY);
+	cv::equalizeHist(gray_img, out);
+};
+
+void sharpen(cv::Mat& input, cv::Mat& output)
+{
+	// by Ivano
+    cv::Mat imgLaplacian;
+    //cv::Laplacian(input, imgLaplacian, CV_32F);
+    cv::GaussianBlur(input, output, cv::Size(5,5),0);
+    cv::Laplacian(output, imgLaplacian, CV_32F);
+    output.convertTo(output, CV_32F);
+    output = output - imgLaplacian;
+    // convert back to 8bits gray scale
+    output.convertTo(output, CV_8U);
+}
+
+double median(cv::Mat& img)
+{
+	// https://gist.github.com/heisters/9cd68181397fbd35031b
+	double m = (img.rows*img.cols) / 2;
+        int bin = 0;
+        double med = -1.0;
+
+        int histSize = 256;
+        float range[] = { 0, 256 };
+        const float* histRange = { range };
+        bool uniform = true;
+        bool accumulate = false;
+        cv::Mat hist;
+        cv::calcHist( &img, 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate );
+
+        for ( int i = 0; i < histSize && med < 0.0; ++i )
+        {
+            bin += cvRound( hist.at< float >( i ) );
+            if ( bin > m && med < 0.0 )
+                med = i;
+        }
+
+        return med;
+}
+
+typedef struct {
+    std::vector<cv::KeyPoint>* original;
+    std::vector<cv::KeyPoint>* selected;
+	cv::Mat* img;
+	cv::String* name;
+	unsigned int brushSize = 20;
+	unsigned int count = 0;
+} KPCallbackData;
+
+void updateKPImage(KPCallbackData& data)
+{
+	cv::Mat out;
+	cv::drawKeypoints(*data.img, *data.original, out, cv::Scalar(0,0,255));
+	cv::drawKeypoints(out, *data.selected, out, cv::Scalar(0,255,0));
+	imshow(*data.name, out);
+}
+
+static void selectKPCallback( int event, int x, int y, int flags, void* userdata)
+{
+	KPCallbackData* data = (KPCallbackData*)userdata;
+	const double brush_size = data->brushSize;
+	
+    if( x < 0 || x >= data->img->cols || y < 0 || y >= data->img->rows )
+        return;
+	
+	if( event == cv::EVENT_MOUSEMOVE && (flags & cv::EVENT_FLAG_LBUTTON) ) 
+	{
+		cv::Point2f pt(x, y);
+		for(const auto& kp: *(data->original))
+		{
+			if(cv::norm(pt-kp.pt)<brush_size)
+			{
+				data->selected->push_back(kp);
+			}
+		}
+		data->original->erase( std::remove_if
+		(
+			data->original->begin(), data->original->end(),
+			[pt,brush_size](const cv::KeyPoint& kp)
+			{
+				return cv::norm(pt-kp.pt)<brush_size;
+			}
+		), data->original->end());
+		
+		data->count += 1;
+		if(data->count>10)
+		{
+			updateKPImage(*data);
+			data->count = 0;
+		}
+	}
+	if( event == cv::EVENT_MOUSEMOVE && (flags & cv::EVENT_FLAG_RBUTTON) ) 
+	{
+		cv::Point2f pt(x, y);
+		for(const auto& kp: *(data->selected))
+		{
+			if(cv::norm(pt-kp.pt)<brush_size)
+			{
+				data->original->push_back(kp);
+			}
+		}
+		data->selected->erase( std::remove_if
+		(
+			data->selected->begin(), data->selected->end(),
+			[pt,brush_size](const cv::KeyPoint& kp)
+			{
+				return cv::norm(pt-kp.pt)<brush_size;
+			}
+		), data->selected->end());
+		
+		data->count += 1;
+		if(data->count>10)
+		{
+			updateKPImage(*data);
+			data->count = 0;
+		}
+	}
+	
+	if( event == cv::EVENT_LBUTTONUP || !(flags & cv::EVENT_FLAG_LBUTTON) )
+	{
+		updateKPImage(*data);
+	}
+}
+
+unsigned int selectKeypoints(cv::String name, cv::Mat& img, std::vector<cv::KeyPoint>& keypoints, std::vector<cv::KeyPoint>& selected, unsigned int brushSize)
+{
+	KPCallbackData data;
+	data.original = &keypoints;
+	data.selected = &selected;
+	data.img = &img;
+	data.name = &name;
+	data.brushSize = brushSize;
+	
+	updateKPImage(data);
+	cv::setMouseCallback(name, selectKPCallback, &data);
+	while(true)
+	{
+		int pressed = cv::waitKey(0);
+		if(pressed == 'q')
+		{
+			data.brushSize *= 2;
+			data.brushSize = data.brushSize>100?100:data.brushSize;
+		}
+		else if(pressed == 'a')
+		{
+			data.brushSize /= 2;
+			data.brushSize = data.brushSize<5?5:data.brushSize;
+		}
+		else break;
+	}
+	return data.brushSize;
+}
+
+void selectKeypointsRectangle(cv::String name, cv::Mat& img, std::vector<cv::KeyPoint>& keypoints, std::vector<cv::KeyPoint>& selected)
+{
+	cv::Mat out;
+	bool loop = true;
+	
+	// Loop until the user presses enter (or space) without selecting any ROIs
+	// (or until they press c as indicated in opencv's docs)
+	while(loop)
+	{
+		cv::drawKeypoints(img, keypoints, out, cv::Scalar(0,0,255));
+		cv::drawKeypoints(out, selected, out, cv::Scalar(0,255,0));
+		cv::Rect ROI = cv::selectROI(name, out, false);
+		// stop looping if no ROI is selected
+		if(ROI.x == 0 and ROI.y == 0 and ROI.width == 0 and ROI.height == 0)
+		{
+			loop = false;
+		}
+		// otherwise select the keypoints inside the ROI
+		// and loop again to select the next ROI
+		else
+		{
+			for(const auto& kp: keypoints)
+			{
+				if(ROI.contains(kp.pt))
+				{
+					selected.push_back(kp);
+				}
+			}
+			keypoints.erase( std::remove_if
+			(
+				keypoints.begin(), keypoints.end(),
+				[ROI](const cv::KeyPoint& kp)
+				{
+					return ROI.contains(kp.pt);
+				}
+			), keypoints.end());
+		}
+	}
+}
+
+void findAllKeypoints(cv::Mat& img, std::vector<cv::KeyPoint>& keypoints, bool shouldSharpen)
+{
+	cv::Ptr<cv::Feature2D> sift = cv::SIFT::create();
+	cv::Mat equalized;
+	bgr_to_gray_HE(img, equalized);
+	if(shouldSharpen)
+	{
+		sharpen(equalized, equalized);
+	}
+    sift->detect(equalized, keypoints);
+}
+
+void createMask(cv::Mat& img, std::vector<cv::KeyPoint>& background, std::vector<cv::KeyPoint>& foreground, cv::Mat& mask)
+{
+	bgr_to_gray_HE(img, mask);
+	cv::blur(mask, mask, cv::Size(3,3));
+	
+	double med = median(mask);
+	double lower = 0.67*med;
+	double upper = 1.33*med;
+	lower = lower<0?0:lower;
+	upper = upper>255?255:upper;
+	cv::Canny(mask, mask, lower, upper);
+	cv::bitwise_not(mask, mask);
+	cv::distanceTransform(mask, mask, cv::DIST_L2, 3);
+	cv::normalize(mask, mask, 0, 1.0, cv::NORM_MINMAX);
+	mask.convertTo(mask, CV_8UC3, 255, 0);
+	cv::subtract(cv::Scalar::all(255),mask,mask);
+	
+	// Create the marker image for the watershed algorithm
+    cv::Mat markers = cv::Mat::zeros(mask.size(), CV_32SC1);
+    
+	// Draw the foreground markers
+	for(const auto& kp: foreground)
+	{
+		cv::circle(markers, kp.pt, 3, cv::Scalar(255), -1);
+	}
+	// Draw the background marker
+	for(const auto& kp: background)
+	{
+		cv::circle(markers, kp.pt, 3, cv::Scalar(1), -1);
+	}
+	
+	cv::cvtColor(mask,mask,cv::COLOR_GRAY2BGR);
+	cv::watershed(mask, markers);
+	markers.convertTo(mask, CV_8U);
+}
+
+void showMask(cv::String name, cv::Mat& img, cv::Mat& mask)
+{
+	cv::Mat foreground;
+	cv::Mat background;
+	cv::cvtColor(mask,foreground,cv::COLOR_GRAY2BGR);
+	foreground.convertTo(foreground, CV_32FC3, 0.85/255.0);
+	img.convertTo(background, CV_32FC3, 1/255.0);
+	cv::multiply(foreground, foreground, foreground);
+	cv::multiply(cv::Scalar::all(1.0)-foreground, background, background);
+	cv::add(foreground, background, background);
+	imshow(name,background);
+	cv::waitKey(0);
+}
+
+void saveMask(cv::String baseName, cv::Mat& mask)
+{
+	cv::imwrite(baseName+".png", mask);
 }
